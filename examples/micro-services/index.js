623a7829a7df99243fe25f133baacb5020a655d8
Nidin Vinayakan <01@01alchemist.com>
2019-09-09 14:42:25 +0200

    v.1.2.14

--- examples/micro-services/index.js
+++ examples/micro-services/index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.get("/health", (req, res) => res.json({status:"ok"}));
 app.get("/", (req, res) => res.json(pkg));
+app.get("/version", (req, res) => res.json(pkg.version));
 
 app.listen(pkg.port, () => {
   console.log(`App running on http://0.0.0.0:${pkg.port}`);
