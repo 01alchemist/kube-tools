270daf908fa250938b07c46934794f121467f56b
Nidin Vinayakan <01@01alchemist.com>
2019-09-09 16:06:46 +0200

    printHeader added

--- examples/micro-services/index.js
+++ examples/micro-services/index.js
@@ -5,9 +5,14 @@ delete pkg.scripts
 
 const app = express()
 
+const printHeader = req => console.log(req.headers)
+
 app.get("/health", (req, res) => res.json({status:"ok"}));
 app.get("/", (req, res) => res.json(pkg));
-app.get("/version", (req, res) => res.json(pkg.version));
+app.get("/version", (req, res) => {
+  printHeader(req)
+  res.json(pkg.version)
+});
 
 app.listen(pkg.port, () => {
   console.log(`App running on http://0.0.0.0:${pkg.port}`);
